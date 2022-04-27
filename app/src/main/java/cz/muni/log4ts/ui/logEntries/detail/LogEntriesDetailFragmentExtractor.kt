26628c27f03a4c30f88ccbf505f768168c05b8ee
Michal Čaniga <michalcaniga559@gmail.com>
2022-04-27 18:05:08 +0200

    Add possibility to edit log times in log detail

--- app/src/main/java/cz/muni/log4ts/ui/logEntries/detail/LogEntriesDetailFragmentExtractor.kt
+++ app/src/main/java/cz/muni/log4ts/ui/logEntries/detail/LogEntriesDetailFragmentExtractor.kt
@@ -1,15 +1,26 @@
 package cz.muni.log4ts.ui.logEntries.detail
 
 import cz.muni.log4ts.data.entities.LogEntry
+import cz.muni.log4ts.data.ui.PickedDateTime
 import cz.muni.log4ts.databinding.FragmentLogEditBinding
+import cz.muni.log4ts.extension.toTimestamp
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class LogEntriesDetailFragmentExtractor @Inject constructor() {
-    fun extractUpdatedLogEntry(binding: FragmentLogEditBinding, oldLogEntry: LogEntry): LogEntry {
+    fun extractUpdatedLogEntry(
+        binding: FragmentLogEditBinding,
+        oldLogEntry: LogEntry,
+        pickedStartTime: PickedDateTime,
+        pickedEndTime: PickedDateTime
+    ): LogEntry {
         val name = binding.nameInput.text.toString()
         val project = binding.projectsSpinner.selectedItem.toString()
+
+        val startTime = pickedStartTime.toTimestamp()
+        val endTime = pickedEndTime.toTimestamp()
+        val loggedSeconds = endTime.seconds - startTime.seconds
 
         return LogEntry(
             id = oldLogEntry.id,
@@ -17,9 +28,9 @@ class LogEntriesDetailFragmentExtractor @Inject constructor() {
             name = name,
             namespace = oldLogEntry.namespace,
             project = project,
-            startTime = oldLogEntry.startTime, // TODO: get from binding
-            endTime = oldLogEntry.endTime, // TODO: get from binding
-            loggedSeconds = oldLogEntry.loggedSeconds, // TODO: get from binding
+            startTime = startTime,
+            endTime = endTime,
+            loggedSeconds = loggedSeconds,
         )
     }
 }
